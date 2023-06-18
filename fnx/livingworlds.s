cfc04d21b15b07c274f7361a880f4fddc0e18bf8
ClAndrew <cmlandrews@gmail.com>
2023-06-18 11:56:41 -0700

    Rearrange functions

--- fnx/livingworlds.s
+++ fnx/livingworlds.s
@@ -86,6 +86,185 @@ F256_RESET
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+MAIN
+    LDA #MMU_EDIT_EN
+    STA MMU_MEM_CTRL
+    STZ MMU_IO_CTRL 
+    STZ MMU_MEM_CTRL    
+    LDA #(Mstr_Ctrl_Text_Mode_En|Mstr_Ctrl_Text_Overlay|Mstr_Ctrl_Graph_Mode_En|Mstr_Ctrl_Bitmap_En)
+    STA @w MASTER_CTRL_REG_L 
+    LDA #(Mstr_Ctrl_Text_XDouble|Mstr_Ctrl_Text_YDouble)
+    STA @w MASTER_CTRL_REG_H
+
+    ; Disable the cursor
+    LDA VKY_TXT_CURSOR_CTRL_REG
+    AND #$FE
+    STA VKY_TXT_CURSOR_CTRL_REG
+    
+    JSR ClearScreen        
+             
+    ; Clear to black
+    LDA #$00
+    STA $D00D ; Background red channel
+    LDA #$00
+    STA $D00E ; Background green channel
+    LDA #$00
+    STA $D00F ; Background blue channel
+    
+    STZ TyVKY_BM1_CTRL_REG ; Make sure bitmap 1 is turned off
+    STZ TyVKY_BM2_CTRL_REG ; Make sure bitmap 2 is turned off    
+    LDA #$01 
+    STA TyVKY_BM0_CTRL_REG ; Make sure bitmap 0 is turned on. Setting no more bits leaves LUT selection to 0
+    
+    JSR CopyBitmapLutToDevice
+
+    ; Now copy graphics data
+    lda #<IMG_START ; Set the low byte of the bitmap’s address
+    sta $D101
+    lda #>IMG_START ; Set the middle byte of the bitmap’s address
+    sta $D102
+    lda #`IMG_START ; Set the upper two bits of the address
+    and #$03
+    sta $D103
+
+    ;;;;;;;;;;;;;;;
+
+    ; Set the line number to 0
+    stz line
+
+    ; Calculate the bank number for the bitmap
+    lda #(IMG_START >> 13)
+    sta bm_bank
+bank_loop: 
+    stz dst_pointer ; Set the pointer to start of the current bank
+    lda #$20
+    sta dst_pointer+1
+    ; Set the column to 0
+    stz column
+    stz column+1
+    ; Alter the LUT entries for $2000 -> $bfff
+
+    lda #$80 ; Turn on editing of MMU LUT #0, and use #0
+    sta MMU_MEM_CTRL
+    lda bm_bank
+    sta MMU_MEM_BANK_1 ; Set the bank we will map to $2000 - $3fff
+    stz MMU_MEM_CTRL ; Turn off editing of MMU LUT #0
+
+    ; Fill the line with the color..
+loop2_fillLine
+    lda line ; The line number is the color of the line
+
+    sta (dst_pointer)
+    inc_column: inc column ; Increment the column number
+    bne chk_col
+    inc column+1
+    chk_col: lda column ; Check to see if we have finished the row
+    cmp #<320
+    bne inc_point
+    lda column+1
+    cmp #>320
+    bne inc_point
+
+    LDA line ; If so, increment the line number
+    inc a
+    STA line
+    cmp #240 ; If line = 240, we’re done
+    beq Done_Init
+
+    stz column ; Set the column to 0
+    stz column+1
+    inc_point: inc dst_pointer ; Increment pointer
+    bne loop2_fillLine ; If < $4000, keep looping
+    inc dst_pointer+1
+    lda dst_pointer+1
+    cmp #$40
+    bne loop2_fillLine
+    inc bm_bank ; Move to the next bank
+    bra bank_loop ; And start filling it
+
+Done_Init
+
+    JSR Init_IRQHandler
+    
+    LDA #$01
+    STA AnimationCounter
+
+Lock
+    JSR UpdateLut
+    WAI
+    WAI
+    WAI
+    WAI
+    WAI
+    WAI
+    JMP Lock
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+Init_IRQHandler
+    ; Back up I/O state
+    LDA MMU_IO_CTRL
+    PHA        
+
+    ; Disable IRQ handling
+    SEI
+
+    ; Load our interrupt handler. Should probably back up the old one oh well
+    LDA #<IRQ_Handler
+    STA $FFFE ; VECTOR_IRQ
+    LDA #>IRQ_Handler
+    STA $FFFF ; (VECTOR_IRQ)+1
+
+    ; Mask off all but start-of-frame
+    LDA #$FF
+    STA INT_MASK_REG1
+    AND #~(JR0_INT00_SOF)
+    STA INT_MASK_REG0
+
+    ; Re-enable interrupt handling    
+    CLI
+    PLA ; Restore I/O state
+    STA MMU_IO_CTRL 
+    RTS
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+IRQ_Handler
+    PHP
+    PHA
+    PHX
+    PHY
+    
+    ; Save the I/O page
+    LDA MMU_IO_CTRL
+    PHA
+
+    ; Switch to I/O page 0
+    STZ MMU_IO_CTRL
+
+    ; Check for start-of-frame flag
+    LDA #JR0_INT00_SOF
+    BIT INT_PENDING_REG0
+    BEQ IRQ_Handler_Done
+    
+    ; Clear the flag for start-of-frame
+    STA INT_PENDING_REG0        
+
+    JSR CopyBitmapLutToDevice
+
+IRQ_Handler_Done
+    ; Restore the I/O page
+    PLA
+    STA MMU_IO_CTRL
+    
+    PLY
+    PLX
+    PLA
+    PLP
+    RTI
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ClearScreen
     LDA MMU_IO_CTRL ; Back up I/O page
     PHA
@@ -116,6 +295,67 @@ ClearScreen_ForEach
     STA MMU_IO_CTRL ; Restore I/O page
     RTS
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+CopyBitmapLutToDevice
+    ; Called on startup and every frame during VBLANK.
+
+    ; Switch to page 1 because the lut lives there
+    LDA #1
+    STA MMU_IO_CTRL
+
+    ; Store a dest pointer in $30-$31
+    LDA #<VKY_GR_CLUT_0
+    STA dst_pointer
+    LDA #>VKY_GR_CLUT_0
+    STA dst_pointer+1
+
+    ; Store a source pointer
+    LDA #<LUT_START
+    STA src_pointer
+    LDA #>LUT_START
+    STA src_pointer+1
+
+    LDX #$00
+
+LutLoop
+    LDY #$0
+    
+    LDA (src_pointer),Y
+    STA (dst_pointer),Y
+    INY
+    LDA (src_pointer),Y
+    STA (dst_pointer),Y
+    INY
+    LDA (src_pointer),Y
+    STA (dst_pointer),Y
+
+    INX
+    BEQ LutDone     ; When X overflows, exit
+
+    CLC
+    LDA dst_pointer
+    ADC #$04
+    STA dst_pointer
+    LDA dst_pointer+1
+    ADC #$00 ; Add carry
+    STA dst_pointer+1
+    
+    CLC
+    LDA src_pointer
+    ADC #$04
+    STA src_pointer
+    LDA src_pointer+1
+    ADC #$00 ; Add carry
+    STA src_pointer+1
+    BRA LutLoop
+    
+LutDone
+    ; Go back to I/O page 0
+    STZ MMU_IO_CTRL 
+
+    RTS
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 UpdateLut
@@ -134,7 +374,7 @@ UpdateLut
     SEI
     
     CLC ; Try entering native mode
-    XCE ; xxx
+    XCE ; 
 
     .as
     .xl
@@ -562,246 +802,8 @@ UpdateLutDone
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-IRQ_Handler
-    PHP
-    PHA
-    PHX
-    PHY
-    
-    ; Save the I/O page
-    LDA MMU_IO_CTRL
-    PHA
-
-    ; Switch to I/O page 0
-    STZ MMU_IO_CTRL
-
-    ; Check for start-of-frame flag
-    LDA #JR0_INT00_SOF
-    BIT INT_PENDING_REG0
-    BEQ IRQ_Handler_Done
-    
-    ; Clear the flag for start-of-frame
-    STA INT_PENDING_REG0        
-
-    JSR CopyBitmapLutToDevice
-
-IRQ_Handler_Done
-    ; Restore the I/O page
-    PLA
-    STA MMU_IO_CTRL
-    
-    PLY
-    PLX
-    PLA
-    PLP
-    RTI
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-Init_IRQHandler
-    ; Back up I/O state
-    LDA MMU_IO_CTRL
-    PHA        
-
-    ; Disable IRQ handling
-    SEI
-
-    ; Load our interrupt handler. Should probably back up the old one oh well
-    LDA #<IRQ_Handler
-    STA $FFFE ; VECTOR_IRQ
-    LDA #>IRQ_Handler
-    STA $FFFF ; (VECTOR_IRQ)+1
-
-    ; Mask off all but start-of-frame
-    LDA #$FF
-    STA INT_MASK_REG1
-    AND #~(JR0_INT00_SOF)
-    STA INT_MASK_REG0
-
-    ; Re-enable interrupt handling    
-    CLI
-    PLA ; Restore I/O state
-    STA MMU_IO_CTRL 
-    RTS
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 .include "rsrc/colors.s"
 .include "rsrc/textcolors.s"
-
-MAIN
-    LDA #MMU_EDIT_EN
-    STA MMU_MEM_CTRL
-    STZ MMU_IO_CTRL 
-    STZ MMU_MEM_CTRL    
-    LDA #(Mstr_Ctrl_Text_Mode_En|Mstr_Ctrl_Text_Overlay|Mstr_Ctrl_Graph_Mode_En|Mstr_Ctrl_Bitmap_En)
-    STA @w MASTER_CTRL_REG_L 
-    LDA #(Mstr_Ctrl_Text_XDouble|Mstr_Ctrl_Text_YDouble)
-    STA @w MASTER_CTRL_REG_H
-
-    ; Disable the cursor
-    LDA VKY_TXT_CURSOR_CTRL_REG
-    AND #$FE
-    STA VKY_TXT_CURSOR_CTRL_REG
-    
-    JSR ClearScreen        
-             
-    ; Clear to black
-    LDA #$00
-    STA $D00D ; Background red channel
-    LDA #$00
-    STA $D00E ; Background green channel
-    LDA #$00
-    STA $D00F ; Background blue channel
-    
-    STZ TyVKY_BM1_CTRL_REG ; Make sure bitmap 1 is turned off
-    STZ TyVKY_BM2_CTRL_REG ; Make sure bitmap 2 is turned off    
-    LDA #$01 
-    STA TyVKY_BM0_CTRL_REG ; Make sure bitmap 0 is turned on. Setting no more bits leaves LUT selection to 0
-    
-    JSR CopyBitmapLutToDevice
-
-    ; Now copy graphics data
-    lda #<IMG_START ; Set the low byte of the bitmap’s address
-    sta $D101
-    lda #>IMG_START ; Set the middle byte of the bitmap’s address
-    sta $D102
-    lda #`IMG_START ; Set the upper two bits of the address
-    and #$03
-    sta $D103
-
-    ;;;;;;;;;;;;;;;
-
-    ; Set the line number to 0
-    stz line
-
-    ; Calculate the bank number for the bitmap
-    lda #(IMG_START >> 13)
-    sta bm_bank
-bank_loop: 
-    stz dst_pointer ; Set the pointer to start of the current bank
-    lda #$20
-    sta dst_pointer+1
-    ; Set the column to 0
-    stz column
-    stz column+1
-    ; Alter the LUT entries for $2000 -> $bfff
-
-    lda #$80 ; Turn on editing of MMU LUT #0, and use #0
-    sta MMU_MEM_CTRL
-    lda bm_bank
-    sta MMU_MEM_BANK_1 ; Set the bank we will map to $2000 - $3fff
-    stz MMU_MEM_CTRL ; Turn off editing of MMU LUT #0
-
-    ; Fill the line with the color..
-loop2_fillLine
-    lda line ; The line number is the color of the line
-
-    sta (dst_pointer)
-    inc_column: inc column ; Increment the column number
-    bne chk_col
-    inc column+1
-    chk_col: lda column ; Check to see if we have finished the row
-    cmp #<320
-    bne inc_point
-    lda column+1
-    cmp #>320
-    bne inc_point
-
-    LDA line ; If so, increment the line number
-    inc a
-    STA line
-    cmp #240 ; If line = 240, we’re done
-    beq Done_Init
-
-    stz column ; Set the column to 0
-    stz column+1
-    inc_point: inc dst_pointer ; Increment pointer
-    bne loop2_fillLine ; If < $4000, keep looping
-    inc dst_pointer+1
-    lda dst_pointer+1
-    cmp #$40
-    bne loop2_fillLine
-    inc bm_bank ; Move to the next bank
-    bra bank_loop ; And start filling it
-
-Done_Init
-
-    JSR Init_IRQHandler
-    
-    LDA #$01
-    STA AnimationCounter
-
-Lock
-    JSR UpdateLut
-    WAI
-    WAI
-    WAI
-    WAI
-    WAI
-    WAI
-    JMP Lock
-    
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-CopyBitmapLutToDevice
-    ; Called on startup and every frame during VBLANK.
-
-    ; Switch to page 1 because the lut lives there
-    LDA #1
-    STA MMU_IO_CTRL
-
-    ; Store a dest pointer in $30-$31
-    LDA #<VKY_GR_CLUT_0
-    STA dst_pointer
-    LDA #>VKY_GR_CLUT_0
-    STA dst_pointer+1
-
-    ; Store a source pointer
-    LDA #<LUT_START
-    STA src_pointer
-    LDA #>LUT_START
-    STA src_pointer+1
-
-    LDX #$00
-
-LutLoop
-    LDY #$0
-    
-    LDA (src_pointer),Y
-    STA (dst_pointer),Y
-    INY
-    LDA (src_pointer),Y
-    STA (dst_pointer),Y
-    INY
-    LDA (src_pointer),Y
-    STA (dst_pointer),Y
-
-    INX
-    BEQ LutDone     ; When X overflows, exit
-
-    CLC
-    LDA dst_pointer
-    ADC #$04
-    STA dst_pointer
-    LDA dst_pointer+1
-    ADC #$00 ; Add carry
-    STA dst_pointer+1
-    
-    CLC
-    LDA src_pointer
-    ADC #$04
-    STA src_pointer
-    LDA src_pointer+1
-    ADC #$00 ; Add carry
-    STA src_pointer+1
-    BRA LutLoop
-    
-LutDone
-    ; Go back to I/O page 0
-    STZ MMU_IO_CTRL 
-
-    RTS
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
